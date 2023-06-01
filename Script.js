350895345eedac42430f7bae250fa3cbf659dc8d
gabi3240 <gabi_mun@yahoo.com.ar>
2023-06-01 18:12:24 -0300

    Initial commit

---
+++ Script.js
@@ -0,0 +1,20 @@
+document.getElementById("idheader").innerHTML = `
+<div class="menu">
+          <div class="logo">
+            <a href="Index.html"><img src="img/Logo.jpg"
+      alt="guarderia canina"></a>
+          </div>
+          <nav class="nav">
+            <a href="Index.html"><img src="img/huella.PNG">Inicio</a>
+            <a href="Nosotros.html"><img src="img/huella.PNG">Nosotros</a>
+            <a href="Huéspedes.html"><img src="img/huella.PNG">Huéspedes</a>
+            <a href="Contacto.html"><img src="img/huella.PNG">Contacto</a>
+          </nav>
+          <div class="social">
+          <a href="https://www.facebook.com/Guarder%C3%ADa-Canina-y-Colonia-Pepa-y-Huinca-227859487293519"
+          target="_blank"><i class="fab fa-facebook" aria-hidden="true" style="font-size: 38px; color: rgb(6, 22, 4)"></i></a>
+      </div>
+    </div>`;
+
+document.getElementById("idfooter").innerHTML = `
+<nav class="pie"><h4>Copyright 2021</h4></nav> `;
